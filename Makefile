ef898399406e8f9e073412e1843764a5845d479d
Rakesh Vishvakarma <rakesh.vishvakarma@tmdc.io>
2024-05-14 16:18:59 +0530

    UDPATE: source refresh, timezone and exclude added

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION = 0.42.3-16
+VERSION = 0.42.3-17
 
 .PHONY: cli
 cli: cli.prepare
--- lens2/main.py
+++ lens2/main.py
@@ -7,6 +7,7 @@ def create_board_yaml(lens_meta):
     lens_name = lens_meta['name']
     dir_name = lens_name
     transformed_lens_meta = measure_dimension_sqls(lens_meta)
+    start_iris_board = False
     for table_info in lens_meta['tables']:
         name = table_info['name']
         meta_info = table_info.get('meta', {})
@@ -44,18 +45,25 @@ def create_board_yaml(lens_meta):
                     )
 
                 additional_kv = {}
+                excludes_dimension = []
                 if 'board' in meta_info.keys() or 'iris' in meta_info.keys():
-                    additional_kv.update(meta_info.get('board', {}))
-                    additional_kv.update(meta_info.get('iris', {}))
+                    iris_board_meta = meta_info.get('board', {})
+                    iris_board_meta.update(meta_info.get('iris', {}))
+                    if "excludes" in iris_board_meta.keys():
+                        excludes_dimension = [d.split(".")[1] for d in iris_board_meta['excludes']]
+                        del iris_board_meta['excludes']
+                    additional_kv['available_time_zones'] = lens_meta.get("timeZones", "")
+                    additional_kv.update(iris_board_meta)
                     if 'timeseries' in additional_kv.keys():
                         additional_kv['timeseries'] = additional_kv.get('timeseries', '').split(".")[1]
                 # create board yaml
                 dump_board_yaml(lens_name=dir_name, dimensions=dimensions, measures=measures,
-                                additional_kv=additional_kv, data_obj=data_obj)
+                                additional_kv=additional_kv, data_obj=data_obj, excludes_dimension=excludes_dimension)
+                start_iris_board = True
 
             elif table_info['type'] == 'view' and table_info['public']:
                 data_obj = {"dimensions": [], "name": name, "title": table_info['title']}
-                prefix = False
+                is_prefix = False
                 dimensions = []
                 measures = []
                 existing_dimensions = []
@@ -63,7 +71,6 @@ def create_board_yaml(lens_meta):
                     for dimension in table_info['dimensions']:
                         dimension_name = dimension['name'].split('.')[1]
                         dimension_alias = dimension['aliasMember']
-                        prefix = True if dimension_alias.split('.')[0] in dimension_name else False
                         dimensions.append({
                             "label": dimension['shortTitle'],
                             "description": dimension.get('description', dimension['title']),
@@ -80,7 +87,6 @@ def create_board_yaml(lens_meta):
                         measure_dims_columns = Parser(measure_query).columns  # measure sql columns
                         is_skip = False  # Assume we don't skip by default
                         is_prefix = True if measure_alias.split('.')[0] in measure_name else False
-                        prefix = True if measure_alias.split('.')[0] in measure_name else False
                         for measure_dim_name in measure_dims_columns:
                             if is_prefix:
                                 m_d_name_with_table = f"{measure_alias.split('.')[0]}_{measure_dim_name}"
@@ -100,27 +106,40 @@ def create_board_yaml(lens_meta):
                                                                  table_name=measure_alias.split('.')[0],
                                                                  is_prefix=is_prefix)
                             })
+
                     additional_kv = {}
+                    excludes_dimension = []
                     if 'board' in meta_info.keys() or 'iris' in meta_info.keys():
-                        additional_kv.update(meta_info.get('board', {}))
-                        additional_kv.update(meta_info.get('iris', {}))
+                        iris_board_meta = meta_info.get('board', {})
+                        iris_board_meta.update(meta_info.get('iris', {}))
+                        if "excludes" in iris_board_meta.keys():
+                            if is_prefix:
+                                excludes_dimension = [d.replace(".", "_") for d in iris_board_meta['excludes']]
+                            else:
+                                excludes_dimension = [d.split(".")[1] for d in iris_board_meta['excludes']]
+                            del iris_board_meta['excludes']
+                        additional_kv['available_time_zones'] = lens_meta.get("timeZones", "")
+                        additional_kv.update(iris_board_meta)
                         if 'timeseries' in additional_kv.keys():
-                            if prefix:
+                            if is_prefix:
                                 additional_kv['timeseries'] = additional_kv.get('timeseries', '').replace(".", "_")
                             else:
                                 additional_kv['timeseries'] = additional_kv.get('timeseries', '').split(".")[1]
 
                     dump_board_yaml(lens_name=dir_name, dimensions=dimensions, measures=measures,
-                                    additional_kv=additional_kv, data_obj=data_obj)
+                                    additional_kv=additional_kv, data_obj=data_obj,
+                                    excludes_dimension=excludes_dimension)
+                    start_iris_board = True
                 else:
                     print(f"No dimension found  for view - `{table_info['name']}`")
         else:
             print(f"❌ Skipped {table_info['type']}:{name}")
-    return  
+    return start_iris_board
 
 if __name__ == "__main__":
     meta = get_lens_meta()
-    create_board_yaml(meta)
+    rill_start = create_board_yaml(meta)
 
     # delegate downstream
-    subprocess.call(sys.argv[1:])
+    if rill_start:
+        subprocess.call(sys.argv[1:])
