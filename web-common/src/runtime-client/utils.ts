cbedcbee97fefc4e1bb3af8e62c276c3d004050d
Vineet Agrawal <vineet.agrawal@tmdc.io>
2024-11-21 17:02:24 +0530

    removed console logs

--- web-common/src/runtime-client/utils.ts
+++ web-common/src/runtime-client/utils.ts
@@ -1,12 +1,9 @@
 export function fetchTokenFromLocalStorage() {
   let token;
   const keys = Object.keys(localStorage).filter(key => key.startsWith("modern-oidc"));
-  console.warn("keys========>",keys);
   if(keys?.length>0){
     const storeValue = localStorage.getItem(keys[0]) ? JSON.parse(localStorage.getItem(keys[0])) : {};
-    console.warn("keys========>",storeValue);
     token = storeValue?.access_token;
   }
-  console.warn("keys========>",token);
   return token;
 }
